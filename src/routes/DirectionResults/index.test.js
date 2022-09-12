11c8cf409e3bce876c122e56baba22a09d3213d4
Maggie <qihua_w@hotmail.com>
2022-09-12 20:36:24 +1000

    update tests for useContext

--- src/routes/DirectionResults/index.test.js
+++ src/routes/DirectionResults/index.test.js
@@ -2,41 +2,42 @@ import { render, screen } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 
 import DirectionResults from '.'
+import { DirectionsContext } from '../../contexts/directions.context'
 
 test('shows result when given directions', () => {
-  const mockDirections = {
-    request: {
-      origin: { placeId: 'mock origPlaceId' },
-      destination: { placeId: 'mock destPlaceId' },
-      travelMode: 'DRIVING',
-    },
-    routes: [
-      {
-        legs: [
-          {
-            distance: {
-              text: '2km',
-              value: 2000,
-            },
-            duration: {
-              text: '5 mins',
-              value: 300,
-            },
-          },
-        ],
-        summary: 'mock road',
+  const directions = {
+    results: {
+      request: {
+        origin: { placeId: 'mock origPlaceId' },
+        destination: { placeId: 'mock destPlaceId' },
+        travelMode: 'DRIVING',
       },
-    ],
-    status: 'OK',
-  }
-
-  const mockIconList = {
-    DRIVING: 'mock icon',
+      routes: [
+        {
+          legs: [
+            {
+              distance: {
+                text: '2km',
+                value: 2000,
+              },
+              duration: {
+                text: '5 mins',
+                value: 300,
+              },
+            },
+          ],
+          summary: 'mock road',
+        },
+      ],
+      status: 'OK',
+    },
   }
 
   render(
     <BrowserRouter>
-      <DirectionResults results={mockDirections} iconList={mockIconList} />
+      <DirectionsContext.Provider value={{ directions }}>
+        <DirectionResults />
+      </DirectionsContext.Provider>
     </BrowserRouter>
   )
   const btnElement = screen.getByTestId('direction-result')
